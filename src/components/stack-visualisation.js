fc88d8776f7378f20e9b4f2260f5ef368bc84e36
skcheongbrian <skcheongbrian@gmail.com>
2023-08-06 14:50:05 +0800

    Make groups grow upwards

--- src/components/stack-visualisation.js
+++ src/components/stack-visualisation.js
@@ -10,21 +10,31 @@ const StackVisualisation = ({ stackFrames }) => {
     // Define the size and position of the visualization
     const width = 400;
     const height = 300;
-    const margin = { top: 20, right: 20, bottom: 20, left: 20 };
-    // const innerWidth = width - margin.left - margin.right;
-    // const innerHeight = height - margin.top - margin.bottom;
+    const margin = { top: 40, right: 20, bottom: 20, left: 20 };
+    const innerWidth = width - margin.left - margin.right;
+    const innerHeight = height - margin.top - margin.bottom;
 
     // Clear the existing visualization
     svg.selectAll('*').remove();
 
-    // Create a group for each stack frame
+    // Create a group for each stack frame (grows from bottom up)
     const frameGroups = svg
       .selectAll('.frame-group')
       .data(stackFrames)
       .enter()
       .append('g')
       .attr('class', 'frame-group')
-      .attr('transform', (d, i) => `translate(${margin.left}, ${margin.top + i * 60})`);
+      .attr('transform', (d, i) => `translate(${margin.left}, ${innerHeight - (i + 1) * 80})`);
+
+
+    // Add text labels for function names (above variables)
+    frameGroups
+      .append('text')
+      .attr('class', 'function-name')
+      .attr('x', 10) // Positioned above the variables
+      .attr('y', -10) // Above the variables
+      .text((d) => d.functionName) // Display the functionName for each stack frame
+      .attr('fill', 'white');
 
     // Create a box for each variable in each frame
     frameGroups
@@ -50,6 +60,9 @@ const StackVisualisation = ({ stackFrames }) => {
       .attr('y', (d, i) => i * 25 + 15)
       .text((d) => `${d[0]}: ${d[1]}`)
       .attr('fill', 'white');
+
+    // Update the SVG container's height to fit the growing groups
+    svg.attr('width', width).attr('height', height);
   }, [stackFrames]);
 
   return (
